0b7a266dec692d99e69df4ad8f31c726843d9032
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-03-09 08:00:19 +0300

    350. Intersection of Two Arrays II

--- 350. Intersection of Two Arrays II.cpp
+++ 350. Intersection of Two Arrays II.cpp
@@ -4,20 +4,23 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> intersection(vector<int> &nums1, vector<int> &nums2) {
+    vector<int> intersect(vector<int> &nums1, vector<int> &nums2) {
         sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
-        bool taken[1001] = {};
-        vector<int> ans;
+        int a[1001] = {}, b[1001] = {};
         int i = 0, j = 0, l1 = nums1.size(), l2 = nums2.size();
         while (i < l1 and j < l2) {
             if (nums1[i] == nums2[j]) {
-                !taken[nums1[i]] ? ans.emplace_back(nums1[i]) : 1;
-                taken[nums1[i]] = true;
+                a[nums1[i]]++;
+                b[nums2[j]]++;
                 i++;
                 j++;
             } else if (nums1[i] < nums2[j])i++;
             else if (nums1[i] > nums2[j])j++;
+        }
+        vector<int> ans;
+        for (i = 0; i < 1001; ++i) {
+            ans.insert(ans.end(), min(a[i], b[i]), i); // Add same value multiple times
         }
         return ans;
     }
@@ -33,7 +36,7 @@ int main() {
  * Time Complexity: O(n)
  * Space Complexity: O(n)
  *
- * problem: https://leetcode.com/problems/intersection-of-two-arrays/description/
- * solution: https://leetcode.com/problems/intersection-of-two-arrays/submissions/1198161241/
+ * problem: https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+ * solution: https://leetcode.com/problems/intersection-of-two-arrays-ii/submissions/1198180740/
  *
  */
