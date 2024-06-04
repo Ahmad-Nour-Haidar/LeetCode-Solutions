3f9d51219caa449e4914c3528e36d1aaaf2aa959
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-06-04 09:37:59 +0300

    409. Longest Palindrome

--- 100 -- 999/409. Longest Palindrome.cpp
+++ 100 -- 999/409. Longest Palindrome.cpp
@@ -4,12 +4,18 @@ using namespace std;
 
 class Solution {
 public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+
     int longestPalindrome(const string &s) {
         int a[123] = {};
         for (auto it :s)a[it]++;
         int ans = 0;
         bool one = false;
-        for (auto it :a) {
+        for (auto &it :a) {
             ans += (it - (it & 1));
             one |= (it & 1);
         }
@@ -29,6 +35,6 @@ int main() {
  * Space Complexity: O(1)
  *
  * problem: https://leetcode.com/problems/longest-palindrome/description/
- * solution: https://leetcode.com/problems/longest-palindrome/solutions/4840749/beats-100-00-of-users-with-c-easy/
+ * solution: https://leetcode.com/problems/longest-palindrome/solutions/5256616/beats-100-00-of-users-with-c/
  *
  */
