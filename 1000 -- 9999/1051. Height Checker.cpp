c9425c49a77bac46b856ab4894361664062fb3d6
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-06-10 08:25:21 +0300

    int heightChecker(vector<int>& heights) {

---
+++ 1000 -- 9999/1051. Height Checker.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+
+    int heightChecker(const vector<int> &heights) {
+        vector<int> sorted = heights;
+        sort(sorted.begin(), sorted.end());
+        int ans = 0, n = sorted.size();
+        for (int i = 0; i < n; ++i) {
+            ans += sorted[i] not_eq heights[i];
+        }
+        return ans;
+    }
+};
+
+int main() {
+}
+
+
+/**
+ * profile: https://leetcode.com/Ahmad_Nour_Haidar/
+ *
+ * Time Complexity: O(n * log(n))
+ * Space Complexity: O(n)
+ *
+ * problem: https://leetcode.com/problems/height-checker/description/
+ * solution: https://leetcode.com/problems/height-checker/solutions/5287585/beats-100-00-of-users-with-c/
+ *
+ */
