7930e2a023689eb6a08372f61fe34e10de838984
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-06-13 15:36:09 +0300

    2037. Minimum Number of Moves to Seat Everyone

---
+++ 1000 -- 9999/2037. Minimum Number of Moves to Seat Everyone.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+
+    int minMovesToSeat(vector<int> &seats, vector<int> &students) {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        int ans = 0, n = seats.size();
+        for (int i = 0; i < n; ++i) {
+            ans += abs(seats[i] - students[i]);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
+
+
+/**
+ * profile: https://leetcode.com/Ahmad_Nour_Haidar/
+ *
+ * Time Complexity: O(n * log(n))
+ * Space Complexity: O(1)
+ *
+ * problem: https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/description/
+ * solution: https://leetcode.com/problems/height-checker/solutions/5287585/beats-100-00-of-users-with-c/
+ *
+ */
